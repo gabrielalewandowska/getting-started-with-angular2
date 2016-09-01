cee6fbc18a874f7dd9b9c5aa244d8e5c4ebaf1bc
thegrid22593 <garrett.sanderson@gmail.com>
2016-08-31 21:20:10 -0700

    wires up template

--- src/app/about/about-friends-list/about-friends.component.ts
+++ src/app/about/about-friends-list/about-friends.component.ts
@@ -6,5 +6,36 @@ import {Component} from '@angular/core';
 })
 
 export class AboutFriends {
+    public friends;
 
+    constructor() {
+
+        this.friends = [
+            {
+                'name': 'Bob',
+                'age': 23,
+                'gender': 'male'
+            },
+            {
+                'name': 'Joe',
+                'age': 26,
+                'gender': 'male'
+            },
+            {
+                'name': 'Sally',
+                'age': 33,
+                'gender': 'female'
+            },
+            {
+                'name': 'Hector',
+                'age': 67,
+                'gender': 'male'
+            },
+            {
+                'name': 'Same',
+                'age': 27,
+                'gender': 'male'
+            }
+        ]
+    }
 }
